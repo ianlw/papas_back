45b2f917c08a1b1bd5227a8ff55121142cb7098c
Ianlw <iancillonn@gmail.com>
2024-11-26 13:04:49 -0500

    dynamic port

--- app.py
+++ app.py
@@ -677,4 +677,5 @@ def predict():
 if __name__ == "__main__":
     if not os.path.exists(app.config["UPLOAD_FOLDER"]):
         os.makedirs(app.config["UPLOAD_FOLDER"])
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
